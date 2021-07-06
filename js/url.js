cbcdadead736dd9153196d72b3478b113d66727d
md suhail saifi <kafeel.saifi0987@gmail.com>
2021-07-06 11:55:09 +0530

    update code

--- js/url.js
+++ js/url.js
@@ -1,13 +1,34 @@
 const urlInp=document.querySelector(".url input");
 previewArea=document.querySelector(".preview-area");
 imgtag=previewArea.querySelector(".thumbnail");
+button=document.querySelector(".btn");
 
 
 
 urlInp.onkeyup=()=>{
      previewArea.classList.add("active");
      let text=urlInp.value;
-     if(urlInp.indexOf("https://www.youtube.com/watch?v=")!=-1){
+     if(text.indexOf("https://www.youtube.com/watch?v=")!=-1){
+          let videoId=text.split("v=")[1].substring(0,11);
+          let videoThumbnail=`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+          // console.log(videoThumbnail);
+          imgtag.src=videoThumbnail;
+          
+     }else if(text.indexOf("https://youtu.be/")!=-1){
+          let videoId=text.split("be/")[1].substring(0,11);
+          let videoThumbnail=`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+          console.log(videoThumbnail);
+          imgtag.src=videoThumbnail;
+     }else if(text.match(/\.(jpe?g|png|gif|bmp|webp)$/i)){
+          
+          imgtag.src=text;
+     }else{
+          imgtag.src="";
+          previewArea.classList.remove("active");
           
      }
+
+     // }else if(urlInp.match(/\.(jpe?g|png|gif|bmp|webp)$/i)){
+
+     // }
 }
